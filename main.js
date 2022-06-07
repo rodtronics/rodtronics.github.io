6e093cf4797a3a4555c0874e7c1a641828ab3ce7
rodstone <rodtronicsnz@gmail.com>
2022-06-07 17:03:45 +1200

    signifcant changes again managed to fix flexbox now attempting to add crime types

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-var crime = document.createElement("div");
-crime.className="crime";
-crime.innerHTML="ppoopp";
+function Item(id, speaker, country) {
+    this.id = id;
+    this.speaker = speaker;
+    this.country = country;
+  }
+  var myItems = [new Item(1, 'john', 'au'), new Item(2, 'mary', 'us')];
+
+function structOfCrimes(name, requiredNoto, moneyEarned, notoEarned, timeToCompleteInSeconds, timeToCompleteInHours)
+  {
+      this.name=name;
+      this.requiredNoto=requiredNoto;
+      this.moneyEarned=moneyEarned;
+      this.notoEarned=notoEarned;
+      this.timeToCompleteInSeconds=timeToCompleteInSeconds;
+      this.timeToCompleteInHours=timeToCompleteInHours;
+  }
+
+
+
+for (let i = 0; i<20;i++)
+{
+    var crime = document.createElement("div");
+    crime.className="crime";
+
+crime.innerHTML=i;
 
 document.getElementById('crimeID').appendChild(crime);
+
+
+}
