3e8ecb7c72360a5d5951b2672f0fcfc1fc6bcdf9
rodstone <rodtronicsnz@gmail.com>
2022-06-08 06:34:15 +1200

    buttons click
    making progress on handling events from the button click

--- main.js
+++ main.js
@@ -11,11 +11,13 @@ function structOfCrimes(name, requiredNoto, moneyEarned, notoEarned, timeToCompl
 // this creates an array "setOfCrime" that contains different crimes
 var setOfCrime = [
     new structOfCrimes("Loitering", 0, 0, 2, 3, 0),
-    new structOfCrimes("Skateboarding", 10, 0, 5, 10, 0)
+    new structOfCrimes("Skateboarding", 10, 0, 5, 10, 0),
+    new structOfCrimes("Littering",7,0,3,1,0)
+
 ]
 
 // this function builds the crime windows and places them in the flexbox
-function createCrimeWindows(structOfCrimesIterator) {
+function createCrimeWindows(structOfCrimesIterator, buttonIndex) {
 
     // create a new div that will be added to the flexbox
     var newWindowButton = document.createElement("button");
@@ -29,11 +31,21 @@ function createCrimeWindows(structOfCrimesIterator) {
 
     // add the text to the div
     newWindowButton.innerHTML = newWindowText;
-
+    
     // append the newly created div to the flexbox
     document.getElementById('crimeID').appendChild(newWindowButton);
+
+    // add function to the button for when it is clicked
+    document.getElementById(structOfCrimesIterator.name).addEventListener("click",clickOnCrimeButton(buttonIndex));
+}
+
+// this function is called when a crime button is clicked
+function clickOnCrimeButton(idOfButton) {
+    var buttonText = setOfCrime[idOfButton].name;
+    document.getElementById("infoID").textContent = buttonText;
 }
 
 
 
 setOfCrime.forEach(createCrimeWindows);
+document.getElementById("infoID").textContent = "Welcome to Crime Committer";
