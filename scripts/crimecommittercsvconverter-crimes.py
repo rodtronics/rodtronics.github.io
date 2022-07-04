ac9448416ab813bd14b978825c87c5dd1a9f0e2c
rodstone <rodtronicsnz@gmail.com>
2022-07-04 22:14:21 +1200

    v0.9.2
    going to commit and pull into origin
    the neighbourhood update

--- scripts/crimecommittercsvconverter-crimes.py
+++ scripts/crimecommittercsvconverter-crimes.py
@@ -5,7 +5,7 @@ crimelist=""
 with open('data/crimecommitter.csv', newline='') as csvfile:
     crime_reader = csv.reader(csvfile)
     for row in crime_reader:
-        newrow = 'new structOfCrimes("'+row[0] + '","' + row[1]+'",'+row[2]+','+row[3]+','+row[4]+','+row[5]+','+row[6]+','+row[7]+','+row[8]+','+row[9]+','+row[10]+'),\n'
+        newrow = 'new structOfCrimes("'+row[0] + '","' + row[1]+'",'+row[2]+','+row[3]+','+row[4]+','+row[5]+','+row[6]+','+row[7]+','+row[8]+','+row[9]+','+row[10]+','+row[11]+'),\n'
         crimelist = crimelist + newrow
 with open('data/crimecommittercrimes.crimefile','w') as crimefile:
     print(crimelist,file=crimefile)
